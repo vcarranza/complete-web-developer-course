48f7400c8becf34725158cb10bc981d387d5c62f
vcarranza <viccarranza67@gmail.com>
2019-05-01 12:46:03 -0700

    javascript terms

--- javascript-part-of-the-course/script.js
+++ javascript-part-of-the-course/script.js
@@ -26,5 +26,38 @@ alert(12);
 
 var list = ["tiger", "cat", "bear", "bird"];
 
+/* function declaration  */
 
+function newFunction(){
+
+};
+/* function expression */
+var newFunction = function (){
+
+};
+
+/* expression  */
+1*3;
+var a = 2;
+return true;
+
+alert();
+newFunction(param1, param2);
+
+/* assign a variable */
+var a = true;
+
+/* function vs method */
+function thisIsAFunction(){
+
+}
+
+var obj = {
+    thisIsAMethod: function() {
+
+    }
+}
+
+thisIsAFunction()
+obj.thisIsAMethod()
 
